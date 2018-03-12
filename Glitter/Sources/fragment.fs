cd5537c5799ec55f426dcc9b0fc9d55d60f0b064
aldenwels <pgringer@Philips-MBP-2.fios-router.home>
2018-03-11 22:33:00 -0400

    Mixes color based on distance from point defined in this case as (1,1)

--- Glitter/Sources/fragment.fs
+++ Glitter/Sources/fragment.fs
@@ -1,28 +1,22 @@
 #version 410 core
-
+/*
+ Mixes color based on distance from point defined in this case as (1,1)
+ */
 in vec4 gl_FragCoord;
 out vec4 color;
 uniform vec2 resolution = vec2(1200,1600);
-vec3 colorA = vec3(0.149,0.141,0.912);
-vec3 colorB = vec3(1.000,0.833,0.224);
+uniform float time;
 
-float plot(vec2 st, float pct){
-    return  smoothstep( pct-0.02, pct, st.y) -
-    smoothstep( pct, pct+0.02, st.y);
-}
+vec3 colorA = vec3(0.195,0.912,0.027);
+vec3 colorB = vec3(1.000,0.950,0.994);
 
 void main() {
+    
     vec2 st = gl_FragCoord.xy/resolution;
-    
-    vec3 pct = vec3(st.x);
-    vec3 c = vec3(0.0);
-    c = mix(colorA,colorB,pct);
-    
-    // Plot transition lines for each channel
-    c = mix(c,vec3(1.0,0.0,0.0),plot(st,pct.r));
-    c = mix(c,vec3(0.0,1.0,0.0),plot(st,pct.g));
-    c = mix(c,vec3(0.0,0.0,1.0),plot(st,pct.b));
-   
+    float pct = 0.0;
+    pct = distance(st,vec2(1));
+    vec3 c = mix(colorA,colorB,pct);
     color = vec4(c,1.0);
+    
 }
 
